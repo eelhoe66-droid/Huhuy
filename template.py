5b95529966b43eef4120cec34e19c18d2483793c
eelhoe66-droid <eelhoe66@gmail.com>
2026-09-07 02:18:35 +0700

    Create template.py

---
+++ template.py
@@ -0,0 +1,20 @@
+TEMPLATE_DONI = """```
+ㅤ   ‌, ´´; __ , ´´;　‌‌ ‌ ‌ 
+　‌ ;　𓂂 · ˔ · 𓂂 ‌ ‌ ;　‌
+　‌ ´　っ♡ c ‌ ‌ ‌ 𝗦𝗨𝗚𝗔𝗥𝗪𝗛𝗘𝗘𝗣 𝗦𝗔𝗡𝗖𝗧𝗨𝗔𝗥𝗬 ☁️
+ㅤㅤㅤᅠㅤㅤㅤᅠ 
+
+꒰ 🍰 ๋࣭⭑꒱  𝙖 𝙟𝙤𝙮𝙛𝙪𝙡 𝙧𝙚𝙘𝙞𝙥𝙚 𝙜𝙪𝙞𝙙𝙚𝙨 𝙩𝙝𝙚 𝙟𖦹𝙪𝙧𝙣𝙚𝙮...݁ ˖Ი𐑼⋆
+
+in a sanctuary where the world softens x3! ☆ ˖˟ 
+๑˚。🎀 a warm oven meets the coolness of 
+vanilla ice cream with chocolate🍦.* ♡
+
+໒ 𓈒° 🧁 t‌i‌m‌e‌ i‌s‌ s‌a‌v‌o‌r‌e‌d‌ l‌i‌k‌e‌ a‌ s‌l‌o‌w‌ m‌e‌l‌t‌i‌n‌g‌
+c‌h‌o‌c‌o‌l‌a‌t‌e‌ t‌r‌u‌f‌f‌l‌e‌, and every shared ⠾ (❥) 
+𔓐𑇓 .. experience becomes a decadent
+cupcake indulgence. 🥣🥛♪ ྀི
+
+🛋 ◛ ۫ ּ 𝗠𝗘𝗡𝗦𝗜𝗩𝗘𝗥𝗦𝗔𝗥𝗬 𝗗𝗔𝗧𝗘:
+➴ .. https://t.me/wheepylove/12
+```"""
